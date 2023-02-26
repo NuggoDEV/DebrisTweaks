8f2388b2d92921ba549605c449686affbc958e7a
NuggoDEV <NuggoDEV@gmail.com>
2023-02-26 21:12:46 +1030

    Properly readd stripped methods - PC debris broken

--- src/Hooks/DebrisHook.cpp
+++ src/Hooks/DebrisHook.cpp
@@ -17,6 +17,18 @@ using namespace GlobalNamespace;
 #include "UnityEngine/Color.hpp"
 using namespace UnityEngine;
 
+void set_freezeRotation(UnityEngine::Rigidbody *rigidBody, bool value)
+{
+    static auto set_freezeRotation = reinterpret_cast<function_ptr_t<void, Rigidbody*, bool>>(il2cpp_functions::resolve_icall("UnityEngine.Rigidbody::set_freezeRotation"));
+    set_freezeRotation(rigidBody, value);
+}
+
+void set_drag(UnityEngine::Rigidbody *rigidbody, float value)
+{
+    static auto set_drag = reinterpret_cast<function_ptr_t<void, Rigidbody*, float>>(il2cpp_functions::resolve_icall("UnityEngine.Rigidbody::set_drag"));
+    set_drag(rigidbody, value);
+}
+
 MAKE_AUTO_HOOK_MATCH(NoteDebris_Init, &NoteDebris::Init, void, NoteDebris* self, ColorType colorType, Vector3 notePos, Quaternion noteRot, Vector3 noteMoveVec, Vector3 noteScale, Vector3 positionOffset, Quaternion rotationOffset, Vector3 cutPoint, Vector3 cutNormal, Vector3 force, Vector3 torque, float lifeTime)
 {
     if (getPluginConfig().ModToggle.GetValue())
@@ -24,11 +36,11 @@ MAKE_AUTO_HOOK_MATCH(NoteDebris_Init, &NoteDebris::Init, void, NoteDebris* self,
         float velocity = getPluginConfig().VelocityMult.GetValue();
 
         lifeTime = getPluginConfig().OverrideDebrisLifetime.GetValue() ? getPluginConfig().DebrisLifetime.GetValue() : lifeTime;
-        noteScale = Vector3().get_one() * getPluginConfig().DebrisScale.GetValue();
+        /*noteScale = Vector3().get_one() * getPluginConfig().DebrisScale.GetValue();
         
         force.x *= velocity; 
         force.y *= velocity; 
-        force.z *= velocity;
+        force.z *= velocity;*/
     }
 
     NoteDebris_Init(self, colorType, notePos, noteRot, noteMoveVec, noteScale, positionOffset, rotationOffset, cutPoint, cutNormal, force, torque, lifeTime);
@@ -37,10 +49,8 @@ MAKE_AUTO_HOOK_MATCH(NoteDebris_Init, &NoteDebris::Init, void, NoteDebris* self,
     {
         Rigidbody *rb = self->GetComponent<Rigidbody*>();
 
-        static auto set_freezeRotation = reinterpret_cast<function_ptr_t<void, Rigidbody*, bool>>(il2cpp_functions::resolve_icall("UnityEngine::Rigidbody::set_freezeRotation"));
         set_freezeRotation(rb, getPluginConfig().PreventRotations.GetValue());
 
-        static auto set_drag = reinterpret_cast<function_ptr_t<void, Rigidbody*, float>>(il2cpp_functions::resolve_icall("UnityEngine::Rigidbody::set_drag"));
         set_drag(rb, getPluginConfig().DebrisDrag.GetValue());
 
         Transform *transform = self->get_transform();
